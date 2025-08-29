31e43bef1843557e40bbec28166bae5df374377c
El-manu-2025 <manuel.ramos20@inacapmail.cl>
2025-08-29 16:39:23 -0400

    feat mi primera plantilla

--- bienvenida/bienvenida/views.py
+++ bienvenida/bienvenida/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from . import models
+
 def inicio(request):
     return HttpResponse("Hola mundo desde django")
+
+def lista_productos(request):
+    productos = models.Producto.objects.all()
+    return render(request, 'productos/lista.html', {'productos': productos})
