48428a26f23eae1b69838f68de7a2c8523b95a56
El-manu-2025 <manuel.ramos20@inacapmail.cl>
2025-09-05 16:43:04 -0400

    feat: django parte 2

--- bienvenida/inventario/models.py
+++ bienvenida/inventario/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
-    precio = models.DecimalField(default=0)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     descripcion = models.TextField(max_length=256, default='')
     stock = models.IntegerField(default=0)
     
